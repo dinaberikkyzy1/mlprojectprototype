aa6ad98e2dbf11aefaeaa4db51e34c060ed45158
dinaberikkyzy1 <200103045@stu.sdu.edu.kz>
2022-11-03 20:21:51 +0600

    First commit

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromRGBO(74, 71, 138, 1.0);
+const kPrimaryLightColor = Color(0xFFF1E6FF);
+const double defaultPadding = 16.0;
